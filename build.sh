f5422488363c43426f699ae10629e5d940e86a6e
Roman28101 <r.kovalenko28101@gmail.com>
2023-06-12 20:26:48 +0800

    Correct build

--- build.sh
+++ build.sh
@@ -5,16 +5,21 @@ echo "Starting the build process..."
 
 # Run some commands
 echo "Running command 1..."
-command1
+set -o errexit
 
 echo "Running command 2..."
-command2
+pip install -r requirements.txt
+
+echo "Running command 3..."
+python manage.py collectstatic --no-input
+
+echo "Running command 4..."
+python manage.py migrate
 
 # Display a success message
 echo "Build process completed successfully!"
-set -o errexit
 
-pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+
+
+
