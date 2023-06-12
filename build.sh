ad37ea6165dc0626be61c43e59396b22af65a58d
Roman28101 <r.kovalenko28101@gmail.com>
2023-06-12 20:14:53 +0800

    Remove .idea add logs to build

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 # exit on error
+# Start of the build process
+echo "Starting the build process..."
+
+# Run some commands
+echo "Running command 1..."
+command1
+
+echo "Running command 2..."
+command2
+
+# Display a success message
+echo "Build process completed successfully!"
 set -o errexit
 
 pip install -r requirements.txt
